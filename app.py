fd5f6a7c7ea1d3fabc7935c1e2025defdfbc68bc
송재욱 <haensol976520@gmail.com>
2025-02-17 23:03:48 +0900

    env

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
-DB_PORT = int(os.getenv('DB_PORT', '3306'))
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = int(os.getenv('DB_PORT'))
 DB_USER = os.getenv('DB_USER', 'root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '1234')
-DB_NAME = os.getenv('DB_NAME', 'hammer')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 DB_CHARSET = 'utf8mb4'
 
 def get_connection():
