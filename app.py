e2ef31d41be4090b803a5d3fd5071609570289a5
송재욱 <haensol976520@gmail.com>
2025-04-13 00:14:35 +0900

    기본값 설정

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = int(os.getenv('DB_PORT'))
+DB_PORT = int(os.getenv('DB_PORT', '3306'))
 DB_USER = os.getenv('DB_USER', 'root')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
